99c4df41b309216822a64ef0ba014cf28e3f79f7
socke300 <60921761+socke300@users.noreply.github.com>
2021-06-09 12:47:38 +0200

    Options added

--- src/app/profile/options/options.page.ts
+++ src/app/profile/options/options.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-options',
@@ -7,9 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OptionsPage implements OnInit {
 
-  constructor() { }
+  firstname = "Max";
+  lastname = "Mustermann";
+  profileImage = "https://bit.ly/2S904CS";
+  description = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et uptua. Atdolores etat.";
+
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  async backBtn(){
+    const alert = document.createElement('ion-alert');
+    alert.header = 'Änderungen verwefen?';
+    alert.buttons = [{ text: "Ja" }, { text: "Nein" }];
+
+    document.body.appendChild(alert);
+    await alert.present();
+    await alert.onDidDismiss();
+    this.router.navigate(['profile']);
+  }
+
+  payment(){
+
+  }
+
+  profileImageChange(){
+    console.log("Test");
   }
 
 }
