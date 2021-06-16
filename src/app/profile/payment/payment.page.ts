51617a8789dc27716f33043101e6f40d1bfe32cc
socke300 <60921761+socke300@users.noreply.github.com>
2021-06-16 14:44:10 +0200

    Payment modified

--- src/app/profile/payment/payment.page.ts
+++ src/app/profile/payment/payment.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-payment',
@@ -6,10 +7,64 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./payment.page.scss'],
 })
 export class PaymentPage implements OnInit {
+  cash: boolean;
+  ec_card: boolean;
+  kreditcard: boolean;
+  paypal: boolean;
 
-  constructor() { }
+  profileImageOld: boolean;
+  firstnameOld: boolean;
+  lastnameOld: boolean;
+  awardsOld: boolean;
+
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  ionViewWillEnter() {
+    // TODO: Daten Laden aus Service
+    this.profileImageOld = true;
+    this.firstnameOld = true;
+    this.lastnameOld = false;
+    this.awardsOld = false;
+
+    this.cash = this.profileImageOld;
+    this.ec_card = this.firstnameOld;
+    this.kreditcard = this.lastnameOld;
+    this.paypal = this.awardsOld;
+  }
+
+  async backBtn() {
+    if (this.cash != this.profileImageOld || this.ec_card != this.firstnameOld || this.kreditcard != this.lastnameOld || this.paypal != this.awardsOld) {
+      const alert = document.createElement('ion-alert');
+      alert.header = 'Änderungen verwefen?';
+      alert.buttons = [{ text: "Ja", role: "yes" }, { text: "Abbrechen" }];
+
+      document.body.appendChild(alert);
+      await alert.present();
+      var rsl = await alert.onDidDismiss();
+      if (rsl.role == "yes")
+        this.router.navigate(['options']);
+    }
+    else {
+      this.router.navigate(['options']);
+    }
+  }
+
+  async saveBtn() {
+    const alert = document.createElement('ion-alert');
+    alert.header = 'Änderungen speichern?';
+    alert.buttons = [{ text: "Ja", role: "yes" }, { text: "Abbrechen" }];
+
+    document.body.appendChild(alert);
+    await alert.present();
+    var rsl = await alert.onDidDismiss();
+
+    if (rsl.role == "yes")
+      this.router.navigate(['options']);
+    // TODO: Über Service speichern
   }
 
 }
