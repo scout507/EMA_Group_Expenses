3f0eae311f2cac31b2782a509a2f47b168c25569
socke300 <60921761+socke300@users.noreply.github.com>
2021-06-16 14:35:21 +0200

    Privacy site modified

--- src/app/profile/privacy/privacy.page.ts
+++ src/app/profile/privacy/privacy.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-privacy',
@@ -6,10 +7,64 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./privacy.page.scss'],
 })
 export class PrivacyPage implements OnInit {
+  profileImage: boolean;
+  firstname: boolean;
+  lastname: boolean;
+  awards: boolean;
 
-  constructor() { }
+  profileImageOld: boolean;
+  firstnameOld: boolean;
+  lastnameOld: boolean;
+  awardsOld: boolean;
+
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  ionViewWillEnter() {
+    // TODO: Daten Laden aus Service
+    this.profileImageOld = true;
+    this.firstnameOld = true;
+    this.lastnameOld = false;
+    this.awardsOld = false;
+
+    this.profileImage = this.profileImageOld;
+    this.firstname = this.firstnameOld;
+    this.lastname = this.lastnameOld;
+    this.awards = this.awardsOld;
+  }
+
+  async backBtn() {
+    if (this.profileImage != this.profileImageOld || this.firstname != this.firstnameOld || this.lastname != this.lastnameOld || this.awards != this.awardsOld) {
+      const alert = document.createElement('ion-alert');
+      alert.header = 'Änderungen verwefen?';
+      alert.buttons = [{ text: "Ja", role: "yes" }, { text: "Abbrechen" }];
+
+      document.body.appendChild(alert);
+      await alert.present();
+      var rsl = await alert.onDidDismiss();
+      if (rsl.role == "yes")
+        this.router.navigate(['options']);
+    }
+    else {
+      this.router.navigate(['options']);
+    }
+  }
+
+  async saveBtn() {
+    const alert = document.createElement('ion-alert');
+    alert.header = 'Änderungen speichern?';
+    alert.buttons = [{ text: "Ja", role: "yes" }, { text: "Abbrechen" }];
+
+    document.body.appendChild(alert);
+    await alert.present();
+    var rsl = await alert.onDidDismiss();
+
+    if (rsl.role == "yes")
+      this.router.navigate(['options']);
+    // TODO: Über Service speichern
   }
 
 }
