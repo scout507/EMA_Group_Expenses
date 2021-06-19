015c908aaa42d651649620af485070db1f5aeabf
socke300 <60921761+socke300@users.noreply.github.com>
2021-06-19 20:02:55 +0200

    Added User model and service

--- src/app/profile/user.model.ts
+++ src/app/profile/user.model.ts
@@ -1,9 +1,17 @@
 export class User {
 
     constructor(
-        public id: string,
-        public firstname: string,
-        public lastname: string,
-        public image: string,
-        public description: string) { }
+        public id?: string,
+        public firstname?: string,
+        public lastname?: string,
+        public image?: string,
+        public description?: string,
+        public cash?: boolean,
+        public ec_card?: boolean,
+        public kreditcard?: boolean,
+        public paypal?: boolean,
+        public imagePublic?: boolean,   
+        public lastnamePublic?: boolean,   
+        public awardsPublic?: boolean,         
+        ) { }
 }
---
+++ src/app/profile/user.service.ts
@@ -0,0 +1,56 @@
+import { Injectable } from '@angular/core';
+import { User } from './user.model';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class UserService {
+  userCollection: AngularFirestoreCollection<User>;
+
+  constructor(private afs: AngularFirestore) {
+    this.userCollection = afs.collection<User>('User2');
+  }
+
+  persist(user: User) {
+    this.userCollection.add(this.copyAndPrepare(user));
+  }
+
+  findAll() {
+    return this.userCollection.get()
+      .toPromise()
+      .then(snapshot =>
+        snapshot.docs.map(doc => {
+          const user = doc.data();
+          user.id = doc.id;
+          return user;
+        }));
+  }
+
+  findById(id: string) {
+    return this.userCollection.doc(id).get().toPromise().then(res => {
+      const ret = res.data();
+      ret.id = res.id;
+      return ret;
+    });
+  }
+
+  update(user: User) {
+    this.userCollection.doc(user.id).update(this.copyAndPrepare(user));
+  }
+
+  delete(id: string) {
+    this.userCollection.doc(id).delete();
+  }
+
+  private copyAndPrepare(user: User): User {
+    const copy = { ...user };
+    delete copy.id;
+    return copy;
+  }
+
+  findAllSync() {
+    return this.userCollection.valueChanges({ idField: 'id' });
+  }
+}
